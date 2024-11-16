2bcafc97448b51d1891c985e1b647d16ff9e4ee4
dhanshree304 <dhanshreeupadhye0@gmail.com>
2024-11-16 16:08:38 +0530

    app js is commited

---
+++ REACT-201-REVISION/book-/src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import { Navbar } from './Components/Navbar';
+import { AllRoutes } from './Components/AllRoutes';
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <br/>
+    
+     
+      <AllRoutes/>
+     
+      
+    
+    </div>
+  );
+}
+
+export default App;
